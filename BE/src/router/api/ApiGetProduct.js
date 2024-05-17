9be678bef5ba7ee163037050de892409971b2064
k1v4z <addnguyen32gmail.com>
2024-05-17 15:47:36 +0700

    update api get product

--- BE/src/router/api/ApiGetProduct.js
+++ BE/src/router/api/ApiGetProduct.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const { getTikiProduct } = require('../../controller/GetProductController')
 
 const router = express.Router()
 
-const initApiGetProduct = (app) =>{
-    router.get('/tiki-product')
-    app.use('/api/v1/',router)
+const initApiGetProduct = (app) => {
+    router.get('/tiki-product', getTikiProduct)
+    app.use('/api/v1/', router)
 }
 
 module.exports = initApiGetProduct
