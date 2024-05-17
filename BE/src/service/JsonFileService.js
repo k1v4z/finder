8c2854ce16c61be48ad867f2be63ae63031ee0e6
k1v4z <addnguyen32gmail.com>
2024-05-17 15:49:39 +0700

    add json file service

---
+++ BE/src/service/JsonFileService.js
@@ -0,0 +1,18 @@
+//This service regarding write,read and delete data in json file
+var fs = require('fs')
+
+const readData = () => {
+    fs.readFile('../ProductCrawl.json', function (err) {
+        if (err) return console.log(err)
+
+        console.log("The file was saved!")
+    })
+}
+
+const writeData = (nameOfProduct) => {
+    fs.writeFile('../ProductCrawl.json',{nameOfProduct: nameOfProduct},function(err){
+        if (err) return console.log(err)
+
+        console.log("The file was saved!")
+    })
+}
