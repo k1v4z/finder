4e06f7131a78938a8b69af80bb58eda7f704b1bd
k1v4z <addnguyen32gmail.com>
2024-05-17 15:46:35 +0700

    update controller

--- BE/src/controller/GetProductController.js
+++ BE/src/controller/GetProductController.js
@@ -1,3 +1,23 @@
-const getTikiProduct = (req, res) => {
-    //check db if true show product else crawl
+const tikiProduct = require("../service/CrawlTiKi/GetTikiProduct");
+
+const getTikiProduct = async (req, res) => {
+    const name = req.query.name
+
+    const product = await tikiProduct(name)
+    
+    if(product == []){ //if don't have data in database, save to file and craw later
+        
+    }
+
+    try {
+        return res.status(200).json(product)
+    } catch (error) {
+        return res.status(500).json({
+            message: error
+        })
+    }
+}
+
+module.exports = {
+    getTikiProduct
 }
