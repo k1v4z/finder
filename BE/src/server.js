ccf7c306d444ea4c3cf037974447c268d64993fe
k1v4z <addnguyen32gmail.com>
2024-05-17 15:51:10 +0700

    update server file

--- BE/src/server.js
+++ BE/src/server.js
@@ -1,19 +1,28 @@
 require('dotenv').config({ path: '../.env' });
 
 const express = require('express');
+const { merge } = require('./helper/craw_tiki');
 const app = express()
 const port = process.env.PORT;
 const localhost = process.env.HOST;
+const cron = require('node-cron');
+const initApiGetProduct = require('./router/api/ApiGetProduct');
 
 //config req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res) => {
-    res.send('404 NOT Found');
-})
 
+const test = async () => {
+    console.log(await merge())
+}
 
+initApiGetProduct(app)
+
+cron.schedule('35 21 * * *', () => {
+    console.log('Bắt đầu cào dữ liệu vào lúc 11h');
+    test()
+});
 
 app.listen(port, localhost, () => {
     console.log(`http://${localhost}:${port}`);
