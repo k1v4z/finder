e24632bfc5af4b621a427603973b7d6c14730102
k1v4z <addnguyen32gmail.com>
2024-05-21 12:12:12 +0700

    use middleware

--- BE/src/router/api/ApiGetProduct.js
+++ BE/src/router/api/ApiGetProduct.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const { getTikiProduct } = require('../../controller/GetProductController')
 const { getSendoProduct } = require('../../controller/GetProductController')
+const blockEndPoint = require('../../middleware/block')
 
 const router = express.Router()
 
 const initApiGetProduct = (app) => {
-    router.get('/tiki-product', getTikiProduct)
+    router.get('/tiki-product', blockEndPoint, getTikiProduct)
     app.use('/api/v1/', router)
 }
 
 const getSendoProductApi = (app) => {
-    router.get('/sendo-product', getSendoProduct)
+    router.get('/sendo-product', blockEndPoint, getSendoProduct)
     app.use('/api/v1/', router)
 }
 
--- BE/src/router/api/Feedback.js
+++ BE/src/router/api/Feedback.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const feedbackRouter = express.Router()
-const submitFeedback = require('../../controller/FeedbackController')
+const submitFeedback = require('../../controller/FeedbackController');
+const blockEndPoint = require('../../middleware/Block');
+
 feedbackRouter.post('/feedback-submit', submitFeedback);
 
 const feedbackSubmit = (app) => {
-    feedbackRouter.post('/feedback-submit', submitFeedback)
+    feedbackRouter.post('/feedback-submit',blockEndPoint,submitFeedback)
+    feedbackRouter.get('/feebacks-view',)
     app.use('/api/v1/', feedbackRouter)
 }
 module.exports = feedbackSubmit
